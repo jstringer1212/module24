394cfd6d55e37e26518c20033e6e2dfa862a03be
Josh Stringer <jstringer1212@gmail.com>
2024-10-01 01:23:01 -0500

    final commit

--- mod24/src/App.jsx
+++ mod24/src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from 'react'
 import {puppyList} from './data.js'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
-
+  function handleClick() {
+    console.log(puppies.id, puppies.name, "id and name");
+  }
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
   return (
     <div className="App">
       {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
+          {()=>{console.log("puppy id: ", puppy.id)}}
+            return <p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}
+                  </p>
            })
-       }
+      }
+      {featPupId && (
+        <div class="feature">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
